e87fe873f93ef93a539e4919a3f5d03376a40f87
sandipan chatterjee <sandipan.chatterjee8@gmail.com>
2021-04-20 17:06:24 +0530

    guessWord.test.js updated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
+import Input from "./Input";
 function App() {
-  const guessedWords = [
-    {
-      guessedWord: "abc",
-      letterMatchCount: 2,
-    },
-  ];
+  const success = false;
+  const secretWord = "party";
+  const guessedWords = [];
+
   return (
     <div data-test="component-app" className="App">
+      <h1>Jotto</h1>
       <Congrats success={true} />
+      <Input success={success} secret={secretWord} />
       <GuessedWords guessedWords={guessedWords} />
     </div>
   );
--- src/guessWord.test.js
+++ src/guessWord.test.js
@@ -13,14 +13,78 @@ const setup = (state = {}) => {
   inputBox.simulate("change", { target: { value: "train" } });
 
   //simulate click on submit btn
-  const submitBtn = findByAttr(wrapper, "submit-btn");
+  const submitBtn = findByAttr(wrapper, "submit-button");
   submitBtn.simulate("click", { preventDefault() {} });
 
   return wrapper;
 };
 
-describe("no words guessed", () => {});
+describe.only("no words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [],
+    });
+  });
+  test("creates GuessedWords table with one row", () => {
+    const guessedWordRow = findByAttr(wrapper, "guessed-word");
+    expect(guessedWordRow.length).toBe(1);
+  });
+});
 
-describe("some words guessed", () => {});
+describe("some words guessed", () => {
+  let wrapper;
+  let guessedWords = [{ guessedWord: "agile", letterMatchCount: 1 }];
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords,
+    });
+  });
+  test("creates GuessedWords table", () => {
+    const guessedWordRow = findByAttr(wrapper, "guessed-word");
+    expect(guessedWordRow.length).toBe(2);
+  });
+});
 
-describe("guess secret word", () => {});
+describe("guess secret word", () => {
+  let wrapper;
+  let guessedWords = [{ guessedWord: "party", letterMatchCount: 5 }];
+
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: true,
+      guessedWords,
+    });
+    // add value to input box
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "party" } };
+    inputBox.simulate("change", mockEvent);
+
+    // simulate click on submit button
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault() {} });
+  });
+
+  test("add row to guessWords table", () => {
+    const guessWordNodes = findByAttr(wrapper, "guessed-word");
+    expect(guessWordNodes).toHaveLength(3);
+  });
+
+  test("show congrats component", () => {
+    const congratsComponent = findByAttr(wrapper, "component-congrats");
+    expect(congratsComponent.length).toBeGreaterThan(0);
+  });
+
+  test("does not display input component contents", () => {
+    const inputBox = findByAttr(wrapper, "input-box");
+    expect(inputBox.exists()).toBe(false);
+
+    const submitBtn = findByAttr(wrapper, "submit-button");
+    expect(submitBtn.exists()).toBe(false);
+  });
+});
